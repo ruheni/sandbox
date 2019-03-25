f350e76038310a467404d5954840c89161d5b65d
Ruheni Alex <aleruheni@outlook.com>
2019-03-26 00:29:45 +0300

    shadow DOM added
    added styles to news-articles

--- web-components/news-article.js
+++ web-components/news-article.js
@@ -1,6 +1,27 @@
 class NewsArticle extends HTMLElement {
+	// Shadow DOM
+	constructor() {
+		super();
+		this.root = this.attachShadow({ mode: 'open' });
+	}
+
+	// Custom news-article element
 	set article(article) {
-		this.innerHTML = `
+		this.root.innerHTML = `
+            <style>
+                 h4 {
+                    font-family: Georgia, 'Times New Roman', Times, serif;
+                }
+
+                 a, a:visited {
+                    text-decoration: none;
+                    color: inherit;
+                }
+
+                img {
+                    width: 100%;
+                }
+            </style>
             <a href="${article.url}">
                 <h2>${article.title}</h2>
                 <img src="${article.urlToImage || ''}" >
