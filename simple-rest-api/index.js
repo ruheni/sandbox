edbd353c34f2334b79ef599ffee34e6618c233a6
Ruheni Alex <aleruheni@outlook.com>
2019-03-29 01:10:54 +0300

    implemented CRUD routes for the API

--- simple-rest-api/index.js
+++ simple-rest-api/index.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 
 const app = express();
 
-const bear = require('./models/bear');
+const Bear = require('./models/bear');
 // routes for api
 const router = express.Router();
 
@@ -33,6 +33,54 @@ let message = [
 // basic routes
 router.get('/', async (req, res) => {
 	await res.json(message);
+});
+
+router.post('/bears', (req, res) => {
+	let bear = new Bear();
+	bear.name = req.body.name;
+
+	bear.save((err, bears) => {
+		if (err) {
+			res.send(err);
+		}
+		res.json({ message: 'Bear created!' });
+	});
+});
+
+router.get('/bears', (req, res) => {
+	Bear.find((err, bears) => {
+		if (err) res.send(err);
+
+		res.json(bears);
+	});
+});
+
+router.get('/bears/:bear_id', (req, res) => {
+	Bear.findById(req.params.bear_id, (err, bear) => {
+		if (err) res.send(err);
+		res.json(bear);
+	});
+});
+
+router.put('/bears/:bear_id', (req, res) => {
+	Bear.findByIdAndUpdate(req.params.bear_id, (err, bear) => {
+		if (err) res.send(err);
+
+		bear.name = req.body.name;
+
+		bear.save(err => {
+			if (err) res.send(err);
+			res.json({ message: 'Bear updated!' });
+		});
+	});
+});
+
+router.delete('/bears/:bear_id', (req, res) => {
+	Bear.findByIdAndDelete(req.params.bear_id, (err, bear) => {
+		if (err) res.send(err);
+
+		res.json({ message: 'Successfully deleted!' });
+	});
 });
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
