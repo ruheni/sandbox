5010226955c8bbcee8914e6d5e2ec44f64d15531
ruheni <aleruheni@outlook.com>
2019-05-27 22:45:08 +0300

    experimented using worker_threads module

---
+++ nodejs/workerThread.js
@@ -0,0 +1,31 @@
+/**
+ * worker_threads module enables the
+ * use of threads that execute Js in parallel.
+ * Workers are useful for performing CPU-intensive
+ * Js ops. Don't help much with I/O intensive work.
+ */
+const {
+  Worker,
+  isMainThread,
+  parentPort,
+  workerData
+} = require("worker_threads");
+
+if (isMainThread) {
+  module.exports = function parseJsAsync(script) {
+    return new Promise((resolve, reject) => {
+      const worker = new Worker(__filename, { workerData: script });
+      worker.on("message", resolve);
+      worker.on("error", reject);
+      worker.on("exit", code => {
+        if (code !== 0) {
+          reject(new Error(`Worker stopped with exit code ${code}`));
+        }
+      });
+    });
+  };
+} else {
+  const { parse } = require("some-parsing-library");
+  const script = workerData;
+  parentPort.postMessage(parse(script));
+}
